7f2f4526d229b0380630655a2b46c08641b10afd
Vip-Augus <850366301@qq.com>
2019-06-06 20:23:15 +0800

    Initial 添加注释

---
+++ spring-analysis-note/src/main/java/base/label/custom/Apple.java
@@ -0,0 +1,39 @@
+package base.label.custom;
+
+
+/**
+ * 注释 3.1 自定义标签的简单类
+ * @author JingQ at 2019-06-06
+ */
+public class Apple {
+
+	private int id;
+
+	private String unit;
+
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
